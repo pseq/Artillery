8875b3209c13785bdb0d2147a3bfca234379832c
pseq <tataka.post@gmail.com>
2018-12-09 23:38:18 +0300

    Add Tank Turn Around
    + angle & power texts
    + power changing

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -1,39 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunScript : MonoBehaviour {
 
     public GameObject bullet;
-    public float firePower;
+    float firePower = 0;
+    public float firePowerMultipler = 5;
     public GameObject fireSpot;
     public float maxGunAngle = 90;
+    Rigidbody2D bulletRigid;
+    public float forwardDirection;
+    float lastScroll;
+    public float turnaroundScroll = - 0.2f;
+    public Text AngleText;
+    public Text PowerText;
 
     // Use this for initialization
     void Start () {
+        bulletRigid = bullet.GetComponent<Rigidbody2D>();
+        forwardDirection = 1f;
     }
 	
     public void Fire ()
     {
         bullet.transform.SetParent(transform);
-        bullet.GetComponent<Rigidbody2D>().angularVelocity = 0f; 
-        bullet.GetComponent<Rigidbody2D>().rotation = transform.eulerAngles.z;
-        bullet.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-        bullet.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * firePower, ForceMode2D.Impulse);
+        bulletRigid.angularVelocity = 0f;
+        bulletRigid.rotation = transform.eulerAngles.z;
+        bulletRigid.velocity = Vector3.zero;
+        bulletRigid.AddRelativeForce((new Vector2(forwardDirection, 0f)) * firePower, ForceMode2D.Impulse);
         bullet.transform.position = fireSpot.transform.position;
     }
 
     public void GunAngleChange (Vector2 scroll)
     {
-        //Debug.Log(scroll);
-        float gunAngle = (1f - scroll.y) * maxGunAngle + transform.parent.eulerAngles.z;
+        if (scroll.y < turnaroundScroll && lastScroll > turnaroundScroll) TurnAround();
+        lastScroll = scroll.y;
+
+        float gunAngle = (1f - scroll.y) * maxGunAngle * forwardDirection + transform.parent.eulerAngles.z;
         transform.eulerAngles = new Vector3(0f,0f,gunAngle);
+        //AngleText.text = transform.eulerAngles.z.ToString();
+        AngleText.text = (transform.eulerAngles.z + transform.parent.eulerAngles.z).ToString();
     }
 
-    public void TEststst() {
-
+    public void GunPowerChange (Vector2 scroll)
+    {
+        firePower = (1f - scroll.y) * firePowerMultipler;
+        PowerText.text = firePower.ToString();
     }
 
+    public void TurnAround() {
+        forwardDirection *= -1f;
+        transform.parent.localScale = Vector3.Scale(transform.parent.localScale, new Vector3(-1f,1f,1f));
+    }
 
     // Update is called once per frame
     void Update () {
--- Assets/Scripts/TankScript.cs
+++ Assets/Scripts/TankScript.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class TankScript : MonoBehaviour {
 
-    float gunAngle;
-    float maxGunAngle = 90f;
-    Transform gun;
-    Transform body;
-
     // Use this for initialization
     void Start () {
-		gun = gameObject.transform.GetChild(0).GetChild(0);
-        body = gameObject.transform.GetChild(0);
-        Debug.Log(gun.name + " " + gun);
-        //gun.eulerAngles = new Vector3(0f,0f,0f);
 
     }
 
@@ -23,10 +14,5 @@ public class TankScript : MonoBehaviour {
 		
 	}
 
-    public void GunAngleChange (Vector2 scroll)
-    {
-        //Debug.Log(scroll);
-        gunAngle = (1f - scroll.y) * maxGunAngle + body.eulerAngles.z;
-        gun.eulerAngles = new Vector3(0f,0f,gunAngle);
-    }
+
 }
