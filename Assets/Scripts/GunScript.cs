edc5c10d44e5a5d8470229d02fd7f891c2ce26b6
pseq <tataka.post@gmail.com>
2018-12-10 22:58:51 +0300

    Add Sliders Steps

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -2,25 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class GunScript : MonoBehaviour {
 
     public GameObject bullet;
-    float firePower = 0;
-    public float firePowerMultipler = 5;
     public GameObject fireSpot;
-    public float maxGunAngle = 90;
-    Rigidbody2D bulletRigid;
-    public float forwardDirection;
+    public float firePowerMultipler = 6f;
+    public float maxGunAngle = 90f;
+    public float forwardDirection = 1f;
+    public float turnaroundScroll = - 0.1f;
+    public float angleStep = 1f;
+    public float powerStep = 0.1f;
+    public Text angleText;
+    public Text powerText;
+    float firePower;
+    float gunAngle;
     float lastScroll;
-    public float turnaroundScroll = - 0.2f;
-    public Text AngleText;
-    public Text PowerText;
+    Rigidbody2D bulletRigid;
+    AudioSource tick;
 
     // Use this for initialization
     void Start () {
         bulletRigid = bullet.GetComponent<Rigidbody2D>();
-        forwardDirection = 1f;
+        tick = gameObject.GetComponent<AudioSource>();
+
+        gunAngle = transform.eulerAngles.z  + transform.parent.eulerAngles.z;
+        firePower = firePowerMultipler * 0.5f;
     }
 	
     public void Fire ()
@@ -35,29 +43,35 @@ public class GunScript : MonoBehaviour {
 
     public void GunAngleChange (Vector2 scroll)
     {
+        //Turnaround
         if (scroll.y < turnaroundScroll && lastScroll > turnaroundScroll) TurnAround();
         lastScroll = scroll.y;
 
-        float gunAngle = (1f - scroll.y) * maxGunAngle * forwardDirection + transform.parent.eulerAngles.z;
-        transform.eulerAngles = new Vector3(0f,0f,gunAngle);
-        //AngleText.text = transform.eulerAngles.z.ToString();
-        AngleText.text = (transform.eulerAngles.z + transform.parent.eulerAngles.z).ToString();
+        float newGunAngle = (1f - scroll.y) * maxGunAngle * forwardDirection;
+        gunAngle = GunValuechange(gunAngle, newGunAngle, angleStep, angleText);
+        transform.eulerAngles = new Vector3(0f,0f,gunAngle + transform.parent.eulerAngles.z);
     }
 
     public void GunPowerChange (Vector2 scroll)
     {
-        firePower = (1f - scroll.y) * firePowerMultipler;
-        PowerText.text = firePower.ToString();
+        float newFirePower = (1f - scroll.y) * firePowerMultipler;
+        firePower = GunValuechange(firePower, newFirePower, powerStep, powerText);
+    }
+
+    float GunValuechange (float oldVal, float newVal, float step, Text textField) {
+        float delta = Math.Abs(oldVal - newVal);
+        if (delta >= step) {
+            newVal = oldVal - step * ((oldVal - newVal)/Math.Abs(oldVal - newVal));
+            textField.text = newVal.ToString();
+            tick.Play();
+            return newVal;
+        }
+        else return oldVal;
     }
 
     public void TurnAround() {
         forwardDirection *= -1f;
+        gunAngle *= -1f;
         transform.parent.localScale = Vector3.Scale(transform.parent.localScale, new Vector3(-1f,1f,1f));
-    }
-
-    // Update is called once per frame
-    void Update () {
-
-
     }
 }
--- Assets/Scripts/UI/ScrollScript.cs
+++ Assets/Scripts/UI/ScrollScript.cs
@@ -5,29 +5,7 @@ using UnityEngine.UI;
 
 public class ScrollScript : MonoBehaviour {
 
-    ScrollRect scRect;
-    float lastPosition;
-    AudioSource tick;
-    public float tickStep;
-
     // Use this for initialization
     void Start () {
-        scRect = gameObject.GetComponent<ScrollRect>();
-        lastPosition = scRect.verticalNormalizedPosition;
-        tick = gameObject.GetComponent<AudioSource>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void PlayTick()
-    {
-        if (Mathf.Abs(lastPosition - scRect.verticalNormalizedPosition) > tickStep)
-        {
-            tick.Play();
-            lastPosition = scRect.verticalNormalizedPosition;
-        }
     }
 }
