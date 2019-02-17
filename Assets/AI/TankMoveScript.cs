7329f31de8211032d89120d0781ad072f581e64b
pseq <tataka.post@gmail.com>
2019-02-17 20:53:38 +0300

    Wheel Control remake
    reach position not working

--- Assets/AI/TankMoveScript.cs
+++ Assets/AI/TankMoveScript.cs
@@ -7,43 +7,47 @@ public class TankMoveScript : MonoBehaviour
 
     
     public float fuel  = 10000f;
-    public float changePositionDistance  = 1f;
+    public float changePositionDistance  = 20f;
     public float positionSharpness = 1f;
-    public float speed = 1f;
-    WheelJoint2D leftWheel, rightWheel;
-    Rigidbody2D leftWheelRigid, rightWheelRigid;
+    public float speed = 180f;
+    public float motorPower = 1000f;
+
+    public WheelJoint2D[] wheels;
+    JointMotor2D motor;
     public GameObject leftWheelObj, rightWheelObj;
     public int cantMoveFrames = 10;
     public float cantMoveDencity = .5f;
+    //public bool isMoving = false;
+
+    public Transform test;
+    public float oldtestX;
 
     // Start is called before the first frame update
     void Start()
     {
-        //leftWheelObj = transform.GetChild(1).gameObject;
-        //rightWheelObj = transform.GetChild(2).gameObject;
-        rightWheel = rightWheelObj.GetComponent<WheelJoint2D>();
-        leftWheel = leftWheelObj.GetComponent<WheelJoint2D>();
-        rightWheelRigid = rightWheelObj.GetComponent<Rigidbody2D>();
-        leftWheelRigid = leftWheelObj.GetComponent<Rigidbody2D>();
+
+        wheels = GetComponentsInChildren<WheelJoint2D>();
+        //motor.maxMotorTorque = motorPower;
+        //motor.motorSpeed = 270;
+        //foreach(WheelJoint2D wheel in wheels) wheel.motor = motor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //test
+        if(oldtestX != test.position.x) Move(test.position.x);
+        oldtestX = test.position.x;
     }
 
     public float GetTrackReserve() {
         // TODO make this
-        return fuel/speed;
+        return fuel;
     }
 
-    public void Move(float dist) {
+    public void Move(float pos) {
         // Движение в точку
-        // TODO формула вычисления времени из расстояния
-        float time = dist/speed;
-        //Debug.Log("НЕ ТОТ МУВ");
-        //StartCoroutine(MoveCoroutine(time));
+        StartCoroutine(MoveCoroutine(pos));
     }
 
     public void Test(int dir) {
@@ -53,54 +57,84 @@ public class TankMoveScript : MonoBehaviour
 
     public void Move(Direction dir) {
         // Движение в направлении
-        float enemyPos = GetComponent<TankScript>().target.transform.position.x;
+        float tankPos = transform.position.x;
         int side = (int)dir;
-        if (transform.position.x > enemyPos) side *= -1;
-        float time = changePositionDistance/speed * side;
-        StartCoroutine(MoveCoroutine(time));
+        float sign = Mathf.Sign(tankPos - GetComponent<TankScript>().target.transform.position.x);
+        StartCoroutine(MoveCoroutine(tankPos - changePositionDistance * side * sign));
+        //if (tankPos - enemyPos > 0)     StartCoroutine(MoveCoroutine(tankPos - changePositionDistance * side));
+          //                      else    StartCoroutine(MoveCoroutine(tankPos + changePositionDistance * side));
     }
     
     IEnumerator MoveCoroutine(float pos) {
-            // move
-            rightWheelRigid.freezeRotation = false;
-            leftWheelRigid.freezeRotation = false;
-            if (pos > transform.position.x) leftWheel.useMotor = true;
-            else rightWheel.useMotor = true;
-            float lastPosition = transform.position.x;
-            int i = 0;
-            //wait
-            //Debug.Log("TANK START MOVE " + transform.name);
-            while (!TestReach(pos) && fuel > 0) {
-                i++;
-                if (i % cantMoveFrames == 0)  {
-                    Debug.Log("MOVE dens=" + Mathf.Abs(lastPosition - transform.position.x));
-                    if (Mathf.Abs(lastPosition - transform.position.x) < cantMoveDencity) break;
-                    lastPosition = transform.position.x;
-                }
-                FuelDecrease();
-                yield return new WaitForEndOfFrame(); //WaitForSeconds(0.1f); или physicsUpdate
+        // move
+        Direction dir = Direction.Left;
+        if (pos > transform.position.x) dir = Direction.Right;
+        StartMove(dir);
+        
+        float lastPosition = transform.position.x;
+        int i = 0;
+
+        while (!TestReach(pos) && fuel > 0) {
+
+            //cant move test
+            i++;
+            if (i % cantMoveFrames == 0)  {
+                Debug.Log("MOVE dens=" + Mathf.Abs(lastPosition - transform.position.x));
+                if (Mathf.Abs(lastPosition - transform.position.x) < cantMoveDencity) break;
+                lastPosition = transform.position.x;
             }
-            // stop
-            rightWheelRigid.freezeRotation = true;
-            leftWheelRigid.freezeRotation = true;
-            rightWheel.useMotor = false;
-            leftWheel.useMotor = false;
+            yield return new WaitForEndOfFrame(); //WaitForSeconds(0.1f); или physicsUpdate
+        }
+        // stop
+        Debug.Log("NOW STOP");
+        StopMove();
+    }
+    
+    IEnumerator FuelDecreaseCoroutine() {
+        while (true) {
+            FuelDecrease();
+            yield return new WaitForEndOfFrame(); //WaitForSeconds(0.1f); или physicsUpdate
+        }
     }
 	
+    //NOT WORKING
     bool TestReach(float pos) {
-                return(Mathf.Abs(pos - transform.position.x) < positionSharpness);
+        //Debug.Log(Mathf.Abs(pos - transform.position.x));
+        return(Mathf.Abs(pos - transform.position.x) < positionSharpness);
     }
 	
     void FuelDecrease() {
          fuel--;
-         if (fuel < 0) fuel = 0;   
+         if (fuel < 0) {
+             fuel = 0;
+             StopMove();
+         }   
     }
 
-    public void StartCor(int l) {
-        StartCoroutine(MoveCoroutine(l));
+    public void StartMove(int dir) {
+        StartMove((Direction) dir);
     }
 
-    public void Stopcor() {
-        StopCoroutine(MoveCoroutine(0));
+    public void StartMove(Direction dir) {
+        StopMove();
+        //rightWheelRigid.freezeRotation = false;
+        //leftWheelRigid.freezeRotation = false;
+        motor.maxMotorTorque = motorPower;
+
+        if (dir == Direction.Right) motor.motorSpeed = speed;
+            else motor.motorSpeed = -speed;
+        Debug.Log("MOVE motor.speed=" + motor.motorSpeed);      
+        foreach(WheelJoint2D wheel in wheels) wheel.motor = motor;  
+        StartCoroutine("FuelDecreaseCoroutine");
+    }
+
+    public void StopMove() {
+        //rightWheelRigid.freezeRotation = true;
+        //leftWheelRigid.freezeRotation = true;
+        //rightWheel.useMotor = false;
+        //leftWheel.useMotor = false;
+        motor.motorSpeed = 0;
+        foreach(WheelJoint2D wheel in wheels) wheel.motor = motor;        
+        StopCoroutine("FuelDecreaseCoroutine");
     }
 }
