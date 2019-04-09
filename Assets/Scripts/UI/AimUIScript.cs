5b19a22746ae27397027618983b22c52e81775ae
pseq <tataka.post@gmail.com>
2019-04-09 14:28:25 +0300

    AimGird added and operateable

---
+++ Assets/Scripts/UI/AimUIScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimUIScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas canvas = GetComponent<Canvas>();
+        canvas.worldCamera = Camera.main;
+    }
+
+    public void On() {
+        GetComponent<Animator>().Play("On");
+    }
+
+    public void Off() {
+        GetComponent<Animator>().Play("Off");
+    }
+}
