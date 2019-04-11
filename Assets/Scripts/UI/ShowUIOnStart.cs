c33d544305c2109ff4813c3d7ecbb16184615f66
pseq <tataka.post@gmail.com>
2019-04-11 20:52:27 +0300

    Aim UI OK presumedly

---
+++ Assets/Scripts/UI/ShowUIOnStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUIOnStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animator>().SetTrigger("ShowOnStart");
+    }
+}
