ac5e62ac8e4b6816ae74a1a299214d2d4345baf2
pseq <tataka.post@gmail.com>
2019-05-25 17:37:19 +0300

    Added ZUI

--- Assets/Scripts/UI/Menu/LoadScene.cs
+++ Assets/Scripts/UI/Menu/LoadScene.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    public void LoadByIndex(int sceneIndex)
+    //public void LoadByIndex(int sceneIndex)
+    public void LoadSceneByName(string sceneName)
     {
-        SceneManager.LoadScene (sceneIndex);
+        //SceneManager.LoadScene (sceneIndex);
+        SceneManager.LoadScene(sceneName);
     }
 }
